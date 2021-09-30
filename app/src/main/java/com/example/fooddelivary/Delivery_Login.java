70dd1828c432475a469b605873e29ecbd196926d
Abdullah272056 <abdullah272056@gmail.com>
2021-09-30 09:12:24 +0600

    Initial commit

---
+++ app/src/main/java/com/example/fooddelivary/Delivery_Login.java
@@ -0,0 +1,14 @@
+package com.example.fooddelivary;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Delivery_Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_delivery_login);
+    }
+}
---
+++ app/src/main/java/com/example/fooddelivary/Loginphone.java
@@ -0,0 +1,14 @@
+package com.example.fooddelivary;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Loginphone extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loginphone);
+    }
+}
